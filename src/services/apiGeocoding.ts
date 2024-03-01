92be6eb228d623255b5e574ec7fc5fabd2652c67
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-03-01 12:12:41 +0530

    Starter Files

---
+++ src/services/apiGeocoding.ts
@@ -0,0 +1,14 @@
+type getAddressTypes = {
+  latitude: string;
+  longitude: string;
+};
+
+export async function getAddress({ latitude, longitude }: getAddressTypes) {
+  const res = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+  );
+  if (!res.ok) throw Error('Failed getting address');
+
+  const data = await res.json();
+  return data;
+}
